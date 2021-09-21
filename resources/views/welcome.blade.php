c4281a9359079ad951aefc2fe381516dfc1b9168
nasmus <nasmus.shahadat@gmail.com>
2021-09-21 16:30:22 +0600

    header h1 x-large

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>Md.Nasmus Shahadat</title>
+    <title>md.nasmus shahadat</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
